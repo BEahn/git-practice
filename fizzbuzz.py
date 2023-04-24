f5f1ea595d8c277365fa12484f5e2f99dbeb2568
BEahn <bb453@naver.com>
2023-04-24 21:29:34 +0900

    feat: Python env test
    Create do_fizzbuzz and test.

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+def do_fizzbuzz():
+    """
+    3의 배수일 때 fizz 출력
+    5의 배수일 때 buzz 출력
+    15의 배수일 때 fizzbuzz 출력
+    o.w : number 그대로 출력
+    """
+    print("hello")
+
+if __name__=='__main__':
+    do_fizzbuzz()
