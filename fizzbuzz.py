857516a74800cff68330ac453b6f05b6b43a7046
BEahn <bb453@naver.com>
2023-04-24 21:39:56 +0900

    feat: why no working

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,9 @@
-def do_fizzbuzz():
-    """
-    3의 배수일 때 fizz 출력
-    5의 배수일 때 buzz 출력
-    15의 배수일 때 fizzbuzz 출력
-    o.w : number 그대로 출력
-    """
+def do_fizzbuzz(num:int):
     print("hello")
-
+    for i in range(1,1+num):
+        if i%3==0:
+            print('fizz')
+        else:
+            print(f'{i}')
 if __name__=='__main__':
-    do_fizzbuzz()
+    do_fizzbuzz(16)
