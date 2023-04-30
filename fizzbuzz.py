fcdda5df6ccd70fa9f227db7fe53b31004dc7822
BEahn <bb453@naver.com>
2023-04-30 22:30:49 +0900

    feat: fizzubuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,9 @@
-def do_fizzbuzz(num:int):
+def do_fizzbuzz():
+    """
+    fizzbuzz : print fizz buzz fizzbuzz
+    """
+
     print("hello")
-    for i in range(1,1+num):
-        if i%3==0:
-            print('fizz')
-        else:
-            print(f'{i}')
+
 if __name__=='__main__':
-    do_fizzbuzz(16)
+    do_fizzbuzz()
