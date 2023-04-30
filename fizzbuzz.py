9459af07c7bedc308a7bb14baf8303247eaac604
BEahn <bb453@naver.com>
2023-04-30 22:42:36 +0900

    feat: add fizzbuzz devide on 3

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,13 @@
-def do_fizzbuzz():
+def do_fizzbuzz(num:int):
     """
     fizzbuzz : print fizz buzz fizzbuzz
     """
 
-    print("hello")
+    for i in range(1,num+1):
+        if i%3==0:
+            print('fizz')
+        else:
+            print(f'{i}')
 
 if __name__=='__main__':
-    do_fizzbuzz()
+    do_fizzbuzz(16)
